fffac73b6e40cff72fdcbb7072ba0ba9d29d9421
dpetrukh <dinispetrux@gmail.com>
2026-08-02 21:15:19 +0100

    first_exercises

---
+++ mod0/ex01/format_ft_time.py
@@ -0,0 +1,10 @@
+import time
+
+seconds = time.time()
+
+print(f"Seconds since January 1, 1970: {seconds:,.4f} or {seconds:.2e} in scientific notation")
+
+localtime = time.localtime()
+print(f"{time.strftime('%b %d %Y', localtime)}")
+
+
