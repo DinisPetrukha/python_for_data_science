81b349ac1a3b884de4d3116ad68a84d52f1f6276
dpetrukh <dinispetrux@gmail.com>
2026-08-03 19:06:47 +0100

    ex05 complete

---
+++ mod0/ex05/building.py
@@ -0,0 +1,52 @@
+import sys
+import string
+
+
+def str_analyzer(text: str):
+    total_counter = 0
+    upper_counter = 0
+    lower_counter = 0
+    punct_counter = 0
+    spaces_counter = 0
+    digits_counter = 0
+
+    for i in text:
+        if i.isupper():
+            upper_counter += 1
+        elif i.islower():
+            lower_counter += 1
+        # string.punctuation its a list/collection of specific characters
+        elif i in string.punctuation:
+            punct_counter += 1
+        elif i.isspace():
+            spaces_counter += 1
+        elif i.isdigit():
+            digits_counter += 1
+        total_counter += 1
+
+    print(f"The text contains {total_counter} characters:")
+    print(f"{upper_counter} upper letters")
+    print(f"{lower_counter} lower letters")
+    print(f"{punct_counter} punctuation marks")
+    print(f"{spaces_counter} spaces")
+    print(f"{digits_counter} digits")
+
+
+def main():
+    try:
+        assert len(sys.argv) <= 2, "more than one argument is provided"
+
+        if len(sys.argv) == 2:
+            string = sys.argv[1]
+        else:
+            print("What is the text to count?")
+            string = sys.stdin.readline()
+
+        str_analyzer(string)
+
+    except AssertionError as msg:
+        print(f"AssertionError: {msg}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ mod0/ex05/tester.py
@@ -0,0 +1,52 @@
+from building import str_analyzer
+
+string = "Hello World!"
+print(f"String test:\n{string}")
+str_analyzer(string)
+print("")
+
+string = "Python 3.10"
+print(f"String test:\n{string}")
+str_analyzer(string)
+print("")
+
+
+string = "42 Lisboa!"
+print(f"String test:\n{string}")
+str_analyzer(string)
+print("")
+
+
+string = "!@#$%"
+print(f"String test:\n{string}")
+str_analyzer(string)
+print("")
+
+string = "abc 123"
+print(f"String test:\n{string}")
+str_analyzer(string)
+print("")
+
+string = "Code & Data 2026"
+print(f"String test:\n{string}")
+str_analyzer(string)
+print("")
+
+string = "    "
+print(f"String test:\n{string}")
+str_analyzer(string)
+print("")
+
+string = "Yes, we can."
+print(f"String test:\n{string}")
+str_analyzer(string)
+print("")
+
+string = "100% Correct?"
+print(f"String test:\n{string}")
+str_analyzer(string)
+print("")
+
+string = "Do code build products"
+print(f"String test:\n{string}")
+str_analyzer(string)
