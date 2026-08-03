21d1e976cab03f1c8e5e85d1a4772b430f2b3f6c
dpetrukh <dinispetrux@gmail.com>
2026-08-03 12:53:17 +0100

    ex03complete

--- mod0/ex03/NULL_not_found.py
+++ mod0/ex03/NULL_not_found.py
@@ -1,4 +1,16 @@
 def NULL_not_found(object: any) -> int:
-    print(f"{type(object)}")
+    if type(object) == type(None):
+        print(f"Nothing: {object} {type(object)}")
+    elif type(object) == float and object != object:
+        print(f"Cheese: {object} {type(object)}")
+    elif type(object) == int and object == 0:
+        print(f"Zero : {object} {type(object)}")
+    elif type(object) == str and object == "":
+        print(f"Empty: {object} {type(object)}")
+    elif type(object) == bool and object == False:
+        print(f"Fake: {object} {type(object)}")
+    else:
+        print("Type not Found")
+        return 1
 
     return 0
