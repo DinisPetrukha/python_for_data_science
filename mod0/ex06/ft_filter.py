2097c7f1702531fc0de90f09ca8acaec547a3623
dpetrukh <dinispetrux@gmail.com>
2026-08-04 11:19:47 +0100

    complete part1

--- mod0/ex06/ft_filter.py
+++ mod0/ex06/ft_filter.py
@@ -1,14 +1,36 @@
-import sys
+def iterable_function1(it):
+    if it % 2 == 0:
+        return True
 
-# def iterable_function1(it):
 
+def ft_filter(func, iterable):
+    """filter(function or None, iterable) --> filter object
 
-# def ft_filter(func, iterable):
+Return an iterator yielding those items of iterable for which function(item)
+is true. If function is None, return the items that are true."""
+    if func is None:
+        newlist = [it for it in iterable if it]
+    else:
+        newlist = [it for it in iterable if func(it)]
+    return newlist
 
 
 def main():
-	print(filter.__doc__)
+    print("---Original definition:")
+    print(filter.__doc__)
+    print("\n---Homemade definition:")
+    print(ft_filter.__doc__)
+    # li1 = [1, 2, 3, 4, 5, 6, 7, 8]
+    # li2 = [0, 1, False, 2, "", 3, None]
+    li3 = ["Ana", "Tiago", "Mateus", False, True, 1]
 
+    print("---Original Filter")
+    filtered = filter(None, li3)
+    print(list(filtered))
+    print()
+    print("---Homemade Filter")
+    filtered = ft_filter(None, li3)
+    print(list(filtered))
 
 
 if __name__ == "__main__":
